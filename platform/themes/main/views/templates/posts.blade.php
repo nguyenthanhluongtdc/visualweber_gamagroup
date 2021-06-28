3a4bd8b1b48f61e07f4d5138c97c00586cf9e4c0
phung.pn <phung.pn@visualweber.com>
2021-06-28 12:58:40 +0700

    no message

--- platform/themes/main/views/templates/posts.blade.php
+++ platform/themes/main/views/templates/posts.blade.php
@@ -1,6 +1,56 @@
 
 <div class="all-news-content">
-   //code tin tức ở đây
-
-
+<div class="container">
+   <div class="new-section1">
+      <div class="row">
+          <div class="col-lg-4">
+              <h3 class="font-helve-bold font30">
+               Tin tức
+               <br>
+               mới nhất
+              </h3>
+          </div>
+          <div class="col-lg-8">
+              <div class="desc font18 font-helve">
+               Cùng điểm qua các tin tức mới nhất về những dự án đầu tư của GAMA Group Viet Nam.
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
+<div class="container">
+   <div class="main-slider owl-carousel">
+      <div class="slider-item">
+          <img src="{{ Theme::asset()->url('images/new/slider1.jpg') }}" alt="" class="img-slider">
+          <div class="fade"></div>
+          <div class="content">
+              <h4 class="font-helve-bold font20">
+               Nhật coi hồi sinh ngành chất bán dẫn là nhiệm vụ quốc gia 
+              </h4>
+              <a href="" class="font-helve font17">Xem tiếp</a>
+          </div>
+      </div>
+      <div class="slider-item">
+          <img src="{{ Theme::asset()->url('images/new/slider1.jpg') }}" alt="" class="img-slider">
+          <div class="fade"></div>
+          <div class="content">
+            <h4 class="font-helve-bold font20">
+               Nhật coi hồi sinh ngành chất bán dẫn là nhiệm vụ quốc gia 
+              </h4>
+              <a href="" class="font-helve font17">Xem tiếp</a>
+          </div>
+      </div>
+      <div class="slider-item">
+          <img src="{{ Theme::asset()->url('images/new/slider1.jpg') }}" alt="" class="img-slider">
+          <div class="fade"></div>
+          <div class="content">
+               <h4 class="font-helve-bold font20">
+               Nhật coi hồi sinh ngành chất bán dẫn là nhiệm vụ quốc gia 
+              </h4>
+              <a href="" class="font-helve font17">Xem tiếp</a>
+          </div>
+      </div>
+  </div>
+</div>
+{{-- <h1>dfghjk</h1> --}}
 </div>
