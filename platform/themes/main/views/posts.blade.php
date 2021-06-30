bcf1ab942d41ac99d8374d7977fa43848f740bc3
phung.pn <phung.pn@visualweber.com>
2021-06-30 16:28:01 +0700

    no message

--- platform/themes/main/views/posts.blade.php
+++ platform/themes/main/views/posts.blade.php
@@ -8,12 +8,12 @@
             <div class="row">
                 <div class="col-lg-4">
                     <h3 class="font-helve-bold font30 new-title">
-                        {!! $page -> description !!}
+                        {!! $page->description !!}
                     </h3>
                 </div>
                 <div class="col-lg-8">
                     <div class="desc font18 font-helve">
-                        {!! $page -> content!!}
+                        {!! $page->content !!}
 
                     </div>
                 </div>
@@ -26,9 +26,9 @@
             <div class="new--banner">
                 <div class="main-slider owl-carousel">
                     <div class="new--slider__item">
-                        <img src="{{ Theme::asset()->url('images/new/slider1.jpg') }}" alt="" class="img-slider">
-                        <div class="fade">dfghjk</div>
+                        <img src="{{ Theme::asset()->url('images/new/slide.jpg') }}" alt="" class="img-slider">
                         <div class="content">
+                            <div class="content--infor">
                             <h4 class="font-helve-bold font20">
                                 Nhật coi hồi sinh ngành chất bán dẫn là nhiệm vụ quốc gia
                             </h4>
@@ -42,10 +42,11 @@
                                 sứ mệnh quốc gia, quan trọng không kém...</span>
                         </div>
                     </div>
+                    </div>
                     <div class="new--slider__item">
-                        <img src="{{ Theme::asset()->url('images/new/slider1.jpg') }}" alt="" class="img-slider">
-                        <div class="fade"></div>
+                        <img src="{{ Theme::asset()->url('images/new/slide.jpg') }}" alt="" class="img-slider">
                         <div class="content">
+                            <div class="content--infor">
                             <h4 class="font-helve-bold font20">
                                 Nhật coi hồi sinh ngành chất bán dẫn là nhiệm vụ quốc gia
                             </h4>
@@ -59,10 +60,11 @@
                                 sứ mệnh quốc gia, quan trọng không kém...</span>
                         </div>
                     </div>
+                    </div>
                     <div class="new--slider__item">
-                        <img src="{{ Theme::asset()->url('images/new/slider1.jpg') }}" alt="" class="img-slider">
-                        <div class="fade"></div>
+                        <img src="{{ Theme::asset()->url('images/new/slide.jpg') }}" alt="" class="img-slider">
                         <div class="content">
+                            <div class="content--infor">
                             <h4 class="font-helve-bold font20">
                                 Nhật coi hồi sinh ngành chất bán dẫn là nhiệm vụ quốc gia
                             </h4>
@@ -75,6 +77,7 @@
                                 là
                                 sứ mệnh quốc gia, quan trọng không kém...</span>
                         </div>
+                    </div>
                     </div>
                 </div>
             </div>
@@ -90,62 +93,60 @@
                         Xem tin theo
                     </h4>
                 </div>
-                <div class="row mt-3"  >
+                <div class="row mt-3">
 
                     <div class="col-md-3 filter--option  mt-2">
-                       
-                    <div  class="menu-container">
-                        <nav>
-                        <ul class="menu font18 font-helve">
-                        <li class="dropdown dropdown-1">
-                            Mới nhất
-                        <img src="{{ Theme::asset()->url('images/new/dropdown.png') }}" alt="" class="img-slider">
 
-                            <ul class="dropdown_menu dropdown_menu--animated dropdown_menu-3 font18 font-helve">
-                            <li class="dropdown_item-1">Item 1</li>
-                            <li class="dropdown_item-2">Item 2</li>
-                            <li class="dropdown_item-3">Item 3</li>
-                            <li class="dropdown_item-4">Item 4</li>
-                            <li class="dropdown_item-5">Item 5</li>
-                            </ul>
-                        </li>
-                            
-                        </ul>
-                    </nav>
+                        <div class="menu-container">
+                            <nav>
+                                <ul class="menu font18 font-helve">
+                                    <li class="dropdown dropdown-1">
+                                        Mới nhất
+                                        <img src="{{ Theme::asset()->url('images/new/dropdown.png') }}" alt=""
+                                            class="img-slider">
+                                            @php $postNew =  get_post_new(5);  @endphp
+                                            @if($postNew->count())
+                                            @foreach($postNew as $itemPost)
+                                        {{-- @if ($post->count()) --}}
+                                        {{-- @foreach ($post as $itemPost) --}}
+                                        <ul
+                                            class="dropdown_menu dropdown_menu--animated dropdown_menu-3 font18 font-helve">
+                                            {{-- <li class="dropdown_item-1">{{!$itemPost->name}}</li> --}}
+                                            <a href="{{ $itemPost->url }}">   <li class="dropdown_item-2">{{ $itemPost->name }}</li></a>
+                                            {{-- <li class="dropdown_item-3">Item 3</li>
+                                            <li class="dropdown_item-4">Item 4</li>
+                                            <li class="dropdown_item-5">Item 5</li> --}}
+                                        </ul>
+                                    </li>
+                                    @endforeach
+                            @endif
+                                </ul>
+                                {{-- @endforeach --}}
+                                {{-- @endif --}}
+                            </nav>
+                        </div>
                     </div>
-                    
-  
-
-                    </div>
-
                     <div class="col-md-3 filter--option mt-2">
-                            
-                    <div  class="menu-container">
-                        <nav>
-                        <ul class="menu font18 font-helve">
-                            
-                        <li class="dropdown dropdown-1">
-                            Tin tổng hợp
-                           
-                        <img src="{{ Theme::asset()->url('images/new/dropdown.png') }}" alt="" class="img-slider">
-                        @php $categories =  get_all_categories();  @endphp
-                        {{-- @dd($categories) --}}
-                        @if($categories->count())
-                        @foreach($categories as $itemPost)
-                            <ul class="dropdown_menu dropdown_menu--animated dropdown_menu-6 font18 font-helve">
-                            <li class="dropdown_item-1">Tin kinh doanh</li>
-                            <li class="dropdown_item-2">Tin cộng đồng</li>
-                            <li class="dropdown_item-3">Tin nội bộ</li>
-                            {{-- @if (!$itemPost->name) --}}
-                          
-                            </ul>
-                            @endforeach
-                        @endif
-                        </li>
-                        </ul>
-                    </nav>
-                    </div>
-                    
+                        <div class="menu-container">
+                            <nav>
+                                <ul class="menu font18 font-helve">
+                                    <li class="dropdown dropdown-1">
+                                        Tin tổng hợp
+                                        <img src="{{ Theme::asset()->url('images/new/dropdown.png') }}" alt=""
+                                            class="img-slider">
+                                        @php $categories =  get_all_categories();  @endphp
+                                        @if ($categories->count())
+                                            @foreach ($categories as $itemPost)
+                                                <ul class="dropdown_menu dropdown_menu--animated dropdown_menu-6 font18 font-helve">
+                                                    <li class="dropdown_item-3">{{ !$itemPost->name }}</li>
+                                                </ul>
+                                            @endforeach
+                                        @endif
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+
                         {{-- <select class="dropdown new--dropdown">
                             <option hidden>Tin tổng hợp</option>
                             <option value="1">Tin kinh doanh</option>
@@ -155,22 +156,24 @@
                         </select> --}}
                     </div>
                     <div class="col-md-3 filter--option mt-2">
-                        <div  class="menu-container">
+                        <div class="menu-container">
                             <nav>
-                            <ul class="menu font18 font-helve">
-                            <li class="dropdown dropdown-2">
-                                Thương hiệu
-                                 <img src="{{ Theme::asset()->url('images/new/dropdown.png') }}" alt="" class="img-slider">
+                                <ul class="menu font18 font-helve">
+                                    <li class="dropdown dropdown-2">
+                                        Thương hiệu
+                                        <img src="{{ Theme::asset()->url('images/new/dropdown.png') }}" alt=""
+                                            class="img-slider">
 
-                                <ul class="dropdown_menu dropdown_menu--animated dropdown_menu-2 font18 font-helve">
-                                <li class="dropdown_item-1">Item 1</li>
-                                <li class="dropdown_item-2">Item 2</li>
-                                <li class="dropdown_item-3">Item 3</li>
+                                        <ul
+                                            class="dropdown_menu dropdown_menu--animated dropdown_menu-2 font18 font-helve">
+                                            <li class="dropdown_item-1">Item 1</li>
+                                            <li class="dropdown_item-2">Item 2</li>
+                                            <li class="dropdown_item-3">Item 3</li>
+                                        </ul>
+                                    </li>
+
                                 </ul>
-                            </li>
-                                
-                            </ul>
-                        </nav>
+                            </nav>
                         </div>
                     </div>
                 </div>
@@ -184,36 +187,39 @@
         <div class="container">
             <div class="new">
                 <div class="row ">
-                        @php $postNew =  get_post_new(9);  @endphp
-                    @if($postNew->count())
-                    @foreach($postNew as $itemPost)
-                    <div class="col-md-4 mt-2 mb-3 " >
-
-                    <a href="{{ $itemPost->url }}" class="post__overlay">
-                        <img src="{{ RvMedia::getImageUrl($itemPost->image) }}" alt="{{ $itemPost->name }}">
-                    </a>
-                        <a href="{{ $itemPost->url }}" class="post__overlay">
-                            <span class="new--title font-helve font20">{{ $itemPost->name }}</span>
-                        </a>
-                        <div class="new--time font-helve font12">
-                            @if (!$itemPost->categories->isEmpty())
+                    @php $posts  =  get_post_new(9);  @endphp
+                    @if ($posts->count())
+                        @foreach ($posts as $itemPost)
+                            <div class="col-md-4 mt-2 mb-3 ">
+                                <div class="post-thumbnail">
+                                <a href="{{ $itemPost->url }}" class="post__overlay ">
+                                    <img src="{{ RvMedia::getImageUrl($itemPost->image) }}"
+                                        alt="{{ $itemPost->name }}">
+                                </a>
+                            </div>
+                                <a href="{{ $itemPost->url }}" class="post__overlay">
+                                    <span class="new--title font-helve font20">{{ $itemPost->name }}</span>
+                                </a>
+                                <div class="new--time font-helve font12">
+                                    @if (!$itemPost->categories->isEmpty())
                                         <span class="new--info">
-                                            <a href="{{ $itemPost->categories->first()->url }}">{{ $itemPost->categories->first()->name }}</a>
+                                            <a
+                                                href="{{ $itemPost->categories->first()->url }}">{{ $itemPost->categories->first()->name }}</a>
                                         </span>
                                     @endif
-                            <span class="new--item">{{ $itemPost->created_at->format('d/m/Y H:i') }}</span>
-                        </div>
-                        <a href="{{ $itemPost->url }}" class="post__overlay">
-                        <span class="new--des font-helve font18">
-                            {{ $itemPost->description }}
-                            
-                 </span>
-                </a>
-                        </div>
+                                    <span class="new--item">{{ $itemPost->created_at->format('d/m/Y H:i') }}</span>
+                                </div>
+                                <a href="{{ $itemPost->url }}" class="post__overlay">
+                                    <span class="new--des font-helve font18">
+                                        {{ $itemPost->description }}
 
-                            @endforeach
-                            @endif
-                        </div>
+                                    </span>
+                                </a>
+                            </div>
+
+                        @endforeach
+                    @endif
+                </div>
 
             </div>
         </div>
@@ -236,14 +242,13 @@
     </div>
 </div>
 <script src="">
-    $("li").mouseover(function(){
- $(this).find('.drop-down').slideDown(300);
- $(this).find(".accent").addClass("animate");
- $(this).find(".item").css("color","#FFF");
-}).mouseleave(function(){
-  $(this).find(".drop-down").slideUp(300);
-   $(this).find(".accent").removeClass("animate");
-   $(this).find(".item").css("color","#000");
-});
-
+    $("li").mouseover(function() {
+        $(this).find('.drop-down').slideDown(300);
+        $(this).find(".accent").addClass("animate");
+        $(this).find(".item").css("color", "#FFF");
+    }).mouseleave(function() {
+        $(this).find(".drop-down").slideUp(300);
+        $(this).find(".accent").removeClass("animate");
+        $(this).find(".item").css("color", "#000");
+    });
 </script>
