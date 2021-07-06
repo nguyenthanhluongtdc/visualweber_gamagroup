f985517df634299000b7f26cf269b288cfbc4c80
phung.pn <phung.pn@visualweber.com>
2021-07-06 11:48:30 +0700

    sass

--- platform/themes/main/views/talent-detail.blade.php
+++ platform/themes/main/views/talent-detail.blade.php
@@ -5,9 +5,9 @@
         <div class="about-section1">
             <div class="row">
                 <div class="col-lg-12">
-                    <h3 class="font-helve-bold font30">
+                    <h1 class="font-helve-bold font30">
                         {!! $page->description !!}
-                    </h3>
+                    </h1>
                 </div>
             </div>
         </div>
--- platform/themes/main/views/talent-opportunity.blade.php
+++ platform/themes/main/views/talent-opportunity.blade.php
@@ -2,13 +2,13 @@
 
 <div class="all-news-content">
     <div class="container">
-        <div class="about-section1">
+        <div class="new-section1">
             <div class="row">
                 <div class="col-lg-4">
-                    <h3 class="font-helve-bold font30">
+                    <h1 class="new-title font-helve-bold font30">
                         {!! $page -> description !!}
                         
-                    </h3>
+                    </h1>
                 </div>
                 <div class="col-lg-8">
                     <div class="desc font18 font-helve">
@@ -18,7 +18,7 @@
             </div>
         </div>
         </div>
-    </div>
+    </div>s
 {{-- ------------------------banner talent-opportunity ----------------}}
     <div class="opportunity-s2">
         <img src="{{ Theme::asset()->url('images/talent/banner2.jpg') }}" alt="" class="img-slider">
