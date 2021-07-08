4eab4683d8ca7504ac207b07ed6319af9538e60d
phung.pn <phung.pn@visualweber.com>
2021-07-08 10:29:50 +0700

    no message

--- platform/themes/main/views/partner-detail.blade.php
+++ platform/themes/main/views/partner-detail.blade.php
@@ -47,38 +47,38 @@
 <div class="partner-orona">
     <div class="container">
         <div class="row">
-            <h2 class="col-md-4 font-helve font30">
+            <h2 class="col-md-4 partner-title  font-helve-bold font30">
                 Đặc điểm ưu việt <br>
                 của thang máy Orona
             </h2>
             <div class="col-md-8">
                 <div class="row">
                     <div class="col-md-6">
-                        <h3 class="font-helve-bold font20">An Toàn Tuyệt Đối</h3>
+                        <h3 class="partner-title font-helve-bold font20">An Toàn Tuyệt Đối</h3>
                        <p class="font-helve font18">Thang máy Orona được thiết kế và sản xuất tại Tây Ban Nha với yêu cầu khắt khe về tiêu chuẩn an toàn.</p>
                      </div>
                     <div class="col-md-6">
-                        <h3 class="font-helve-bold font20">May đo theo thực tế công trình</h3>
+                        <h3 class="partner-title font-helve-bold font20">May đo theo thực tế công trình</h3>
                        <p class="font-helve font18"> Thang máy Orona được thiết kế theo thực tế từng công trình với thiết kế rất bé, độ sâu Pit ngắn (chỉ từ 300mm) đỉnh giếng thang rất thấp (từ 2600mm) và rất phù hợp cho nhà cải tạo bị khống chế chiều cao.</p>  
                     </div>
                 </div>
-                <div class="row">
+                <div class="row partner-orona__item">
                     <div class="col-md-6">
-                        <h3 class="font-helve-bold font20">Chất lượng xanh Châu Âu</h3>
+                        <h3 class="partner-title font-helve-bold font20">Chất lượng xanh Châu Âu</h3>
                         <p class="font-helve font18">Orona là công ty thang máy duy nhất không có nhà máy ở nước thứ 3, tất cả các linh kiện đều được sản xuất ở G7, từ khâu lựa chọn vật liệu đến sản xuất, đạt tiêu chuẩn sử dụng về môi trường xanh.</p>
                     </div>
                     <div class="col-md-6">
-                       <h3 class="font-helve-bold font20">Dịch vụ bảo hành, bảo trì toàn cầu</h3> 
-                       <p class="font-helve font18">Hệ thống vi xử lý thông minh giúp trung tâm quản lý vận hành tại Tây Ban Nha có thể hướng dẫn và xử lý kỹ thuật thông qua internet. Tất cả các thang máy Orona được bảo hành toàn cầu chính hãng lên tới 36 tháng. </p>
+                       <h3 class="partner-title font-helve-bold font20">Dịch vụ bảo hành, bảo trì toàn cầu</h3> 
+                       <p class="partner-title font-helve font18">Hệ thống vi xử lý thông minh giúp trung tâm quản lý vận hành tại Tây Ban Nha có thể hướng dẫn và xử lý kỹ thuật thông qua internet. Tất cả các thang máy Orona được bảo hành toàn cầu chính hãng lên tới 36 tháng. </p>
                     </div>
                 </div>
-                <div class="row">
+                <div class="row partner-orona__item">
                     <div class="col-md-6">
-                        <h3 class="font-helve-bold font20">Thiết kế sang trọng, lịch lãm </h3>
+                        <h3 class="partner-title font-helve-bold font20">Thiết kế sang trọng, lịch lãm </h3>
                         <p class="font-helve font18">Phong cách kiến trúc và nghệ thuật của Tây Ban Nha in đậm trong từng chi tiết của thang máy Orona, các nhà thiết kế của Orona tại vùng biển San Sebastian (Địa Trung Hải) xinh đẹp.</p>
                     </div>
                     <div class="col-md-6">
-                    <h3 class="font-helve-bold font20">Hỗ trợ khẩn cấp 24/7</h3> 
+                    <h3 class="partner-title font-helve-bold font20">Hỗ trợ khẩn cấp 24/7</h3> 
                     <p class="font-helve font18">Hệ thống trợ giúp khẩn cấp luôn trực 24/7 kèm theo chức năng gọi khẩn cấp (Emcall) sẽ đảm bảo bạn luôn được liên hệ với bên ngoài đến 5 số điện thoại chỉ bằng một nút nhấn. </p>   
                     </div>
                 </div>
