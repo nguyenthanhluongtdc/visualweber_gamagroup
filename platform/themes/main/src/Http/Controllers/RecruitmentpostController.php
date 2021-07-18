f05bfe27d1b1b51d6ad70496dbaf6f022c0a7566
phung.pn <phung.pn@visualweber.com>
2021-07-18 17:22:24 +0700

    no message

---
+++ platform/themes/main/src/Http/Controllers/RecruitmentpostController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Theme\Main\Http\Controllers;
+
+use Theme;
+use Illuminate\Support\Facades\Log;
+use Platform\RecruitmentPost\Models\RecruitmentPost;
+use Platform\RecruitmentPost\Repositories\Interfaces\RecruitmentPostInterface;
+use Platform\Slug\Repositories\Interfaces\SlugInterface;
+use Platform\Theme\Http\Controllers\PublicController;
+
+class  RecruitmentpostController extends PublicController
+{
+    public function show($slug)
+    {
+        try
+        {
+            $slug = app(SlugInterface::class)->getFirstBy(['key'=> $slug, 'reference_type' => RecruitmentPost::class]);
+            $RecruitmentPost= app(RecruitmentPostInterface::class)->getFirstBy(['id' => $slug->reference_id]);
+            return Theme::scope('talent', compact('RecruitmentPost'))->render();
+        } catch(\Throwable $err) {
+            Log::error('Có lỗi xảy ra thực hiện chức năng ' . __CLASS__ . '@' . __FUNCTION__, [$err->getMessage()]);
+            abort(500);
+        }
+    }
+}
