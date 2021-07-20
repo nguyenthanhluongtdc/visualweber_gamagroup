19df426561c5ebe4047d0dbd353294a57c75df9e
thuanho195 <boyover055@gmail.com>
2021-07-20 15:40:15 +0700

    sass

--- platform/themes/main/partials/footer.blade.php
+++ platform/themes/main/partials/footer.blade.php
@@ -100,9 +100,9 @@
     smartSpeed: 1000,
     loop: false,
     autoplay: false,
-    dots: true,
+    dots: false,
     nav: true,
-    navText: ["<div class='nav-btn prev-slide'><i class='fas fa-long-arrow-alt-left'></i></div>", "<div class='nav-btn next-slide'><i class='fas fa-long-arrow-alt-right'></i></div>"],
+    navText: ["<div class='nav-btn prev-slide'><i class='fas fa-chevron-left'></i></div>", "<div class='nav-btn next-slide'><i class='fas fa-chevron-right'></i></div>"],
     responsive: {
         0: {
             items: 1
