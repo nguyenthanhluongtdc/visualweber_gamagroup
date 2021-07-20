577da2610df1f2f3a16e3e68fb222cbf8f987b3c
thuanho195 <boyover055@gmail.com>
2021-07-20 19:09:40 +0700

    sass

--- platform/themes/main/views/talent-detail.blade.php
+++ platform/themes/main/views/talent-detail.blade.php
@@ -197,7 +197,9 @@
 
 
         <div class="Recruitment--buttom xem-them font-helve font18">
-            <a href="/{{ get_slug_job() }}" class="primary-a">{{ trans('See more') }}</a>
+            
+            <a href="/{{ get_slug_job() }}" class="primary-a link-vi-detail">{{ trans('See more') }}</a>
+            <a href="/en/{{ get_slug_job() }}" class="primary-a link-en-detail">{{ trans('See more') }}</a>
 
         </div>
     </div>
